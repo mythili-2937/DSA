adb812f899ee53d4f7ac2be60cd743e221f18175
mythili-2937 <mythili2937@gmail.com>
2026-09-07 11:50:01 +0530

    Valid Parenthese is added

---
+++ BalancedParenthesis.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isBalanced(string s)
+{
+    stack<char> st;
+
+    for (char ch : s)
+    {
+        // Opening brackets
+        if (ch == '(' || ch == '{' || ch == '[')
+        {
+            st.push(ch);
+        }
+
+        // Closing brackets
+        else if (ch == ')' || ch == '}' || ch == ']')
+        {
+            if (st.empty())
+                return false;
+
+            char top = st.top();
+            st.pop();
+
+            if ((ch == ')' && top != '(') ||
+                (ch == '}' && top != '{') ||
+                (ch == ']' && top != '['))
+            {
+                return false;
+            }
+        }
+    }
+
+    return st.empty();
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    if (isBalanced(s))
+        cout << "Balanced";
+    else
+        cout << "Not Balanced";
+
+    return 0;
+}
